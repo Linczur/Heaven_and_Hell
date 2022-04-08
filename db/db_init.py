8e69a0f77af2edb3b0218f06504bbe936d42d83a
Daniel Link <12daniel23@gmail.com>
2022-04-08 21:54:33 +0200

    3 commit

--- db/db_init.py
+++ db/db_init.py
@@ -1,10 +1,8 @@
-from db.fly_school import (
-    Base,
-    create_fake_students,
-)
+from db.fly_school import (Base, create_fake_students)
 from db.session import Session
 
-if __name__ == "__main__":
+
+def main():
     session = Session()
     Base.metadata.create_all(session.get_bind())
     create_fake_students(session)
